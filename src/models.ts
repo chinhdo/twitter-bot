fe53fb53af7e77f6d6ae5bc9440f070af817ef52
Chinh Do <cdo@vienxu.com>
2020-05-10 22:10:41 -0400

    HTML report

---
+++ src/models.ts
@@ -0,0 +1,8 @@
+export class Tweet {
+  public id: string | undefined;
+  public created: string | undefined;
+  public likes: number | undefined;
+  public text: string | undefined;
+  public userScreenName: string | undefined;
+  public userFollowers: number | undefined;
+}
